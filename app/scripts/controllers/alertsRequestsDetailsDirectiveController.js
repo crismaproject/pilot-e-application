b8eeb9e5d5eea7b37751eeeb0072b676ca999041
Martin Scholl <martin.scholl@cismet.de>
2014-05-27 11:52:00 +0200

    [issue #16] proper means addition and removal

--- app/scripts/controllers/alertsRequestsDetailsDirectiveController.js
+++ app/scripts/controllers/alertsRequestsDetailsDirectiveController.js
@@ -2,146 +2,135 @@ angular.module('eu.crismaproject.pilotE.controllers')
     .controller('eu.crismaproject.pilotE.controllers.alertsRequestsDetailsDirectiveController',
         [
             '$scope',
-            '$q',
-            '$filter',
-            'ngTableParams',
-            'eu.crismaproject.pilotE.services.OoI',
             'DEBUG',
-            function ($scope, $q, $filter, NgTableParams, ooi, DEBUG) {
+            function ($scope, DEBUG) {
                 'use strict';
-                
+
+                var select2Format;
+
                 if (DEBUG) {
                     console.log('initialising master alerts requests directive controller');
                 }
-                
+
                 $scope.quantity = 1;
 
-                $scope.tableParams = new NgTableParams(
-                    {
-                        page: 1,
-                        count: 10,
-                        sorting: { name: 'asc' }
-                    },
-                    {
-                        total: $scope.rescueMeans ? $scope.rescueMeans.length : 0,
-                        $scope: {$data: {}},
-                        getData: function ($defer, params) {
-                            var resolvedMeans, i;
-
-                            // we have to load every rescue means to ensure proper paging/sorting
-                            resolvedMeans = [];
-                            
-                            if ($scope.rescueMeans) {
-                                for (i = 0; i < $scope.rescueMeans.length; ++i) {
-                                    resolvedMeans[i] = ooi.getRescueMeans().get({meansId: $scope.rescueMeans[i].id}).$promise;
-                                }
-
-                                $q.all(resolvedMeans).then(function (theMeans) {
-                                    var ordered;
-
-                                    ordered = params.filter() ? $filter('filter')(theMeans, params.filter()) : theMeans;
-                                    ordered = params.sorting() ? $filter('orderBy')(ordered, params.orderBy()) : theMeans;
-
-                                    params.total(ordered.length);
-                                    $defer.resolve(ordered.slice(
-                                        (params.page() - 1) * params.count(),
-                                        params.page() * params.count()
-                                    ));
-                                });
-                            } else {
-                                $defer.resolve([]);
-                            }
-                        }
+                select2Format = function (selection) {
+                    try {
+                        return '<span style="margin: 10px;"><img src="' + JSON.parse(selection.id).icon + '"/></span>' + selection.text;
+                    } catch (e) {
+                        // could not parse selection
                     }
-                );
+                };
+
+                $scope.select2Options = {
+                    allowClear: false,
+                    formatResult: select2Format,
+                    formatSelection: select2Format
+                };
 
                 $scope.allRescueMeans = [
                     {
                         'type': 'TRV',
                         'name': 'Technical rescue vehicle',
-                        'icon': ''
+                        'icon': 'img/ambulance_16.png'
                     },
                     {
                         'type': 'FT',
                         'name': 'Fire truck',
-                        'icon': ''
+                        'icon': 'img/ambulance_16.png'
                     },
                     {
                         'type': 'PV',
                         'name': 'Police vehicle',
-                        'icon': ''
+                        'icon': 'img/ambulance_16.png'
                     },
                     {
                         'type': 'RTH',
                         'name': 'Intensive care helicopter',
-                        'icon': ''
+                        'icon': 'img/ambulance_16.png'
                     },
                     {
                         'type': 'RTW',
                         'name': 'Intensive care ambulance',
-                        'icon': ''
+                        'icon': 'img/ambulance_16.png'
                     },
                     {
                         'type': 'NEF',
                         'name': 'Emergency doctor vehicle',
-                        'icon': ''
+                        'icon': 'img/ambulance_16.png'
                     },
                     {
                         'type': 'MTW',
                         'name': 'Personnel transport vehicle',
-                        'icon': ''
+                        'icon': 'img/ambulance_16.png'
                     },
                     {
                         'type': 'San-EL',
                         'name': 'Command vehicle ambulance services',
-                        'icon': ''
+                        'icon': 'img/ambulance_16.png'
                     },
                     {
                         'type': 'UG-San-EL',
                         'name': 'Supporting leading personnel',
-                        'icon': ''
+                        'icon': 'img/ambulance_16.png'
                     },
                     {
                         'type': 'KID',
                         'name': 'Crisis intervention service',
-                        'icon': ''
+                        'icon': 'img/ambulance_16.png'
                     },
                     {
                         'type': 'GW-San',
                         'name': 'Equipment truck medical service',
-                        'icon': ''
+                        'icon': 'img/ambulance_16.png'
                     },
                     {
                         'type': 'KdoW',
                         'name': 'Command vehicle',
-                        'icon': ''
+                        'icon': 'img/ambulance_16.png'
                     },
                     {
                         'type': 'KTW',
                         'name': 'Patient transport ambulance',
-                        'icon': ''
+                        'icon': 'img/ambulance_16.png'
                     }
                 ];
 
-                $scope.$watch('rescueMeans.length', function () {
-                    $scope.tableParams.reload();
-                });
-
                 $scope.selectedOOAMeans = null;
-                $scope.selectedRescueMeans = $scope.allRescueMeans[0];
+                $scope.selectedRescueMeans = JSON.stringify($scope.allRescueMeans[0]);
 
                 $scope.setSelected = function (means) {
                     $scope.selectedRescueMeans = means;
                 };
-                
+
                 $scope.addOOAMeans = function () {
                     // TODO: impl
-                    $scope.selectedOOAMeans;
+//                    $scope.selectedOOAMeans;
                 };
-                
+
                 $scope.addMeans = function () {
-                    // TODO: impl
-                    $scope.quantity;
-                    $scope.selectedRescueMeans;
+                    var doAdd, i, means;
+
+                    if (!$scope.selectedAlertRequest.rescueMeans) {
+                        $scope.selectedAlertRequest.rescueMeans = [];
+                    }
+
+                    doAdd = true;
+                    means = JSON.parse($scope.selectedRescueMeans);
+                    for (i = 0; i < $scope.selectedAlertRequest.rescueMeans.length && doAdd; ++i) {
+                        if ($scope.selectedAlertRequest.rescueMeans[i].type === means.type) {
+                            $scope.selectedAlertRequest.rescueMeans[i].quantity += $scope.quantity;
+                            doAdd = false;
+                        }
+                    }
+
+                    if (doAdd) {
+                        means.quantity = $scope.quantity;
+                        $scope.selectedAlertRequest.rescueMeans.push(means);
+                    }
+                };
+
+                $scope.removeMeans = function (index) {
+                    $scope.selectedAlertRequest.rescueMeans.splice(index, 1);
                 };
             }]);
